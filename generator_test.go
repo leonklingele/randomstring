710ccaa69b1545f17d17df8e057dd3c33ec5b301
Leon Klingele <git@leonklingele.de>
2023-04-02 13:42:58 +0200

    generator: parallelize tests

--- generator_test.go
+++ generator_test.go
@@ -1,6 +1,7 @@
 package randomstring_test
 
 import (
+	"fmt"
 	"testing"
 	"unicode/utf8"
 
@@ -8,38 +9,60 @@ import (
 )
 
 func TestRandomStringLength(t *testing.T) {
+	t.Parallel()
+
 	ls := []int{1, 5, 10, 50, 100}
 	dict := randomstring.CharsASCII
 
 	for _, l := range ls {
-		s, err := randomstring.Generate(l, dict)
-		if err != nil {
-			t.Error(err)
-		}
+		l := l
 
-		if len(s) != l {
-			t.Errorf("unexpected length of generated string: want %d, got %d", l, len(s))
-		}
+		t.Run(fmt.Sprintf("len-%d", l), func(t *testing.T) {
+			t.Parallel()
+
+			s, err := randomstring.Generate(l, dict)
+			if err != nil {
+				t.Error(err)
+			}
+
+			if len(s) != l {
+				t.Errorf("unexpected length of generated string: want %d, got %d", l, len(s))
+			}
+		})
+
 	}
 }
 
 func TestRandomStringInvalidLength(t *testing.T) {
+	t.Parallel()
+
 	ls := []int{-1, 0}
+	dict := randomstring.CharsAlpha
 
 	for _, l := range ls {
-		if _, err := randomstring.Generate(l, randomstring.CharsAlpha); err != randomstring.ErrInvalidLengthSpecified {
-			t.Errorf("unexpected error for length %d: %v", l, err)
-		}
+		l := l
+
+		t.Run(fmt.Sprintf("len-%d", l), func(t *testing.T) {
+			t.Parallel()
+
+			if _, err := randomstring.Generate(l, dict); err != randomstring.ErrInvalidLengthSpecified {
+				t.Errorf("unexpected error for length %d: %v", l, err)
+			}
+		})
 	}
 }
 
 func TestRandomStringInvalidChars(t *testing.T) {
+	t.Parallel()
+
 	if _, err := randomstring.Generate(1, ""); err != randomstring.ErrInvalidDictSpecified {
 		t.Errorf("unexpected error using empty dictionary: %v", err)
 	}
 }
 
 func TestRandomStringWithNonASCII(t *testing.T) {
+	t.Parallel()
+
 	const (
 		dict = "世界"
 		l    = 5
@@ -49,6 +72,7 @@ func TestRandomStringWithNonASCII(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	if got := utf8.RuneCountInString(s); got != l {
 		t.Fatalf("invalid length of string, got %d, want %d", got, l)
 	}
